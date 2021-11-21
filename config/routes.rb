6b3b3ec5c93b9aa344cd90256cbc7cad703eda9f
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-11-21 23:22:15 +0700

    modeling_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
   get "/admin/home", to:"static_pages_admin#home", as: :home
   get "/admin/create", to:"static_pages_admin#create", as: :create
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root "static_pages#home"
+
+    get "home", to: "static_pages#home"
+  end
 end

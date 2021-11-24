96e5a9fa4821cd85e46aba9e2d377ea3231b7a5a
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-11-24 16:22:19 +0700

    view_history_order

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @products = Product.sort_by_created.page(params[:page]).per(Settings.per_10)
+    @products = Product.sort_by_created.page(params[:page]).per(Settings.per_3)
   end
 
   def show
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
   scope :ordered_by_price, ->{order(:total_money)}
+  scope :sort_by_created, ->{order(created_at: :desc)}
   has_many :products, through: :order_details
 
   validates :name_customer, presence: true,
@@ -11,4 +12,6 @@ class Order < ApplicationRecord
   validates :phone_number, presence: true,
             length: {minimum: Settings.validate.length.digist_6},
             format: {with: Settings.regex.PHONE_NUMBER_REGEX}
+
+  enum status: {pending: 0, approve: 1, reject: 2}, _prefix: true
 end

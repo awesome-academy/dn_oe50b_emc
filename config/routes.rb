61ce1d760a177e0fd34ed35becde3700ad1ca7bf
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-11-24 16:22:19 +0700

    show_client_cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root "static_pages#home"
 
-    get "home", to: "static_pages#home"
+    get "home", to: "static_pages#home", as: :home_client
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
 
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users
+    resources :static_pages
+    resources :order_details
     resources :account_activations, only: :edit
     resources :password_resets, only: [:new, :create, :edit, :update]
   end

f612f553e5d2c690e4ae83beb61c18d9282e2161
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-11-29 15:20:00 +0700

    add_product_to_cart

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,6 +1,7 @@
 class OrderDetailsController < ApplicationController
+  before_action :check_order, only: [:index]
+
   def index
-    check_order
     @orderdetails = @orders.order_details.includes(:product)
   end
 
@@ -9,5 +10,8 @@ class OrderDetailsController < ApplicationController
   def check_order
     @orders = Order.find_by user_id: current_user.id
     return if @orders
+
+    flash[:warning] = t "flash.product_not_found"
+    redirect_to home_client_path
   end
 end

30363ea363ce0d3affc4e7fd1bacc4d0bdb43444
thanghd-1196 <ho.duc.thang@sun-asterisk.com>
2021-12-16 10:05:22 +0700

    API for CRUD in Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace "api" do
+    namespace "v1" do
+      resources :products
+    end
+  end
   devise_for :users, only: :omniauth_callbacks,
               controllers: {omniauth_callbacks: "omniauth_callbacks"}
 

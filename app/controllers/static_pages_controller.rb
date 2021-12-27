d88fda7da3383085938ee339785778d1799f4e4a
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-12-28 02:40:50 +0700

    order_recovery

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  before_action :load_categories, only: %i(home filter_by_category
+                                         filter_by_status)
+
   def home
     @products = @q.result(distinct: true).sort_by_created
                   .page(params[:page]).per(Settings.per_3)
@@ -10,5 +13,23 @@ class StaticPagesController < ApplicationController
 
     flash[:warning] = t "flash.product_not_found"
     redirect_to home_client_path
+  end
+
+  def filter_by_category
+    @products = Product.by_categori(params[:id]).sort_by_created
+                       .page(params[:page]).per(Settings.per_3)
+    render "static_pages/home"
+  end
+
+  def filter_by_status
+    @products = Product.by_status(params[:id]).sort_by_created
+                       .page(params[:page]).per(Settings.per_3)
+    render "static_pages/home"
+  end
+
+  private
+
+  def load_categories
+    @categories = Category.all
   end
 end
